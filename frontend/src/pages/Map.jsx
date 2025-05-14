01527a62a2e7d013ac77b26e8d8d2af8e045d62f
charlesascano <ace0356@dlsud.edu.ph>
2025-05-15 07:28:58 +0800

    Working waste map

--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,19 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Box, Spinner } from "@chakra-ui/react";
 import {
-  Box,
-  Flex
-} from "@chakra-ui/react";
+  GoogleMap,
+  Marker,
+  useJsApiLoader
+} from '@react-google-maps/api';
+import { supabase } from '../lib/supabase';
+
+const GOOGLE_MAPS_API_KEY = 'AIzaSyAGZSYlxM6plLsSAI2lsXpYlw-ZvxMPvTk';
+
+const containerStyle = {
+  width: '100%',
+  height: '100vh'
+};
+
+const center = {
+  lat: 14.406595,
+  lng: 120.940501
+};
 
 export default function Map() {
-  return (
-    <>
-      <Box mt="72px">
-        <Flex>
-          <Box>
-           <h1>WASTE MAP PAGE</h1>
-         </Box>
-        </Flex>
+  const [locations, setLocations] = useState([]);
+
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: GOOGLE_MAPS_API_KEY
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase
+        .from('flotector-data')
+        .select('lat, lng, total_count, result_url');
+
+      if (error) {
+        console.error('Error fetching data:', error);
+      } else {
+        setLocations(data);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (!isLoaded) {
+    return (
+      <Box display="flex" alignItems="center" justifyContent="center" height="100vh">
+        <Spinner size="xl" />
       </Box>
-    </>
+    );
+  }
+
+  return (
+    <Box>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={12}
+      >
+        {locations.map((location, index) => (
+          <Marker
+            key={index}
+            position={{ lat: location.lat, lng: location.lng }}
+            icon={{
+              url:
+                location.total_count >= 5
+                  ? 'https://maps.google.com/mapfiles/ms/icons/red-dot.png'
+                  : 'https://maps.google.com/mapfiles/ms/icons/green-dot.png'
+            }}
+            onClick={() => {
+              window.open(location.result_url, '_blank');
+            }}
+          />
+        ))}
+      </GoogleMap>
+    </Box>
   );
 }
